9ec7c4d10604e2c3650a353922dcf05773b9df39
Junior Avila <junavi2@gmail.com>
2023-12-27 12:01:54 -0400

    init

---
+++ myshop/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-rva(10*_g6lp(v=0oy0z+h=m(yf5e*d7n-ekh&*%_j8yv!yz&f"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
